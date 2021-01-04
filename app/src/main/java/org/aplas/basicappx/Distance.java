116c3afd9ecc508eb17e34fe371f95ff1253858b
atrinasalsabilnengkoda <atrina.nengkoda@gmail.com>
2021-01-04 14:23:25 +0700

    B1 Java

--- app/src/main/java/org/aplas/basicappx/Distance.java
+++ app/src/main/java/org/aplas/basicappx/Distance.java
@@ -1,4 +1,122 @@
 package org.aplas.basicappx;
 
 public class Distance {
+    private double meter;
+
+    public Distance() {
+    }
+
+    public void setMeter(double meter) {
+        this.meter = meter;
+    }
+
+    public double getMeter() {
+        return this.meter;
+    }
+
+    public void setInch(double inch){
+        this.meter = inch/39.3701;
+    }
+
+    public double getInch(){
+        return (this.meter*39.3701);
+    }
+
+    public void setMile(double mile){
+        this.meter = mile/0.000621371;
+    }
+
+    public double getMile(){
+        return (this.meter*0.000621371);
+    }
+
+    public void setFoot(double feet){
+        this.meter = feet/3.28084;
+    }
+
+    public double getFoot(){
+        return (this.meter*3.28084);
+    }
+
+    public double convert(String oriUnit, String convUnit, double value){
+        if (oriUnit.equals("Mtr")){
+            switch (convUnit){
+                case "Mtr":
+                    setMeter(value);
+                    value = getMeter();
+                    break;
+                case "Inc":
+                    setInch(value);
+                    value = getInch();
+                    break;
+                case "Mil":
+                    setMile(value);
+                    value = getMile();
+                    break;
+                case "Ft":
+                    setFoot(value);
+                    value = getFoot();
+                    break;
+            }
+        }else if(oriUnit.equals("Inc")){
+            switch (convUnit){
+                case "Mtr":
+                    setMeter(value);
+                    value = getMeter();
+                    break;
+                case "Inc":
+                    setInch(value);
+                    value = getInch();
+                    break;
+                case "Mil":
+                    setMile(value);
+                    value = getMile();
+                    break;
+                case "Ft":
+                    setFoot(value);
+                    value = getFoot();
+                    break;
+            }
+
+        }else if(oriUnit.equals("Mil")){
+            switch (convUnit){
+                case "Mtr":
+                    setMeter(value);
+                    value = getMeter();
+                    break;
+                case "Inc":
+                    setInch(value);
+                    value = getInch();
+                    break;
+                case "Mil":
+                    setMile(value);
+                    value = getMile();
+                    break;
+                case "Ft":
+                    setFoot(value);
+                    value = getFoot();
+                    break;
+            }
+        }else if(oriUnit.equals("Ft")){
+            switch (convUnit){
+                case "Mtr":
+                    setMeter(value);
+                    value = getMeter();
+                    break;
+                case "Inc":
+                    setInch(value);
+                    value = getInch();
+                    break;
+                case "Mil":
+                    setMile(value);
+                    value = getMile();
+                    break;
+                case "Ft":
+                    setFoot(value);
+                    value = getFoot();
+                    break;
+            }
+        }
+        return value;
+    }
 }
--- app/src/main/java/org/aplas/basicappx/Weight.java
+++ app/src/main/java/org/aplas/basicappx/Weight.java
@@ -1,4 +1,83 @@
 package org.aplas.basicappx;
 
 public class Weight {
+    private double gram;
+
+    public Weight() {
+    }
+
+    public void setGram(double gram) {
+        this.gram = gram;
+    }
+
+    public double getGram() {
+        return gram;
+    }
+
+    public void setOunce(double ounce){
+        this.gram = ounce * 28.3495231;
+    }
+
+    public double getOunce(){
+        return (this.gram / 28.3495231) ;
+    }
+
+    public void setPound(double pound){
+        this.gram = pound * 453.59237;
+    }
+
+    public double getPound(){
+        return (this.gram / 453.59237);
+    }
+
+    public double convert(String oriUnit, String convUnit, double value){
+        if (oriUnit.equals("Grm")){
+            switch (convUnit){
+                case "Grm":
+                    setGram(value);
+                    value = getGram();
+                    break;
+                case "Onc":
+                    setOunce(value);
+                    value = getOunce();
+                    break;
+                case "Pnd":
+                    setPound(value);
+                    value = getPound();
+                    break;
+            }
+        }else if(oriUnit.equals("Onc")){
+            switch (convUnit){
+                case "Grm":
+                    setGram(value);
+                    value = getGram();
+                    break;
+                case "Onc":
+                    setOunce(value);
+                    value = getOunce();
+                    break;
+                case "Pnd":
+                    setPound(value);
+                    value = getPound();
+                    break;
+            }
+
+        }else if(oriUnit.equals("Pnd")){
+            switch (convUnit){
+                case "Grm":
+                    setGram(value);
+                    value = getGram();
+                    break;
+                case "Onc":
+                    setOunce(value);
+                    value = getOunce();
+                    break;
+                case "Pnd":
+                    setPound(value);
+                    value = getPound();
+                    break;
+            }
+        }
+        return value;
+    }
 }
